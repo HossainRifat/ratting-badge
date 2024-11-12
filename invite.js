3f3fa185439ebc4e14cabbd031d6915c9f6a7468
HossainRifat <rh140025@gmail.com>
2024-11-12 18:20:25 +0600

    hello

---
+++ invite.js
@@ -0,0 +1,17 @@
+window.onload = function () {
+    console.log("Hello, World!");
+
+    fetch('https://api.example.com/data')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return response.json(); // Parse the JSON data from the response
+        })
+        .then(data => {
+            console.log(data); // Handle the data from the response
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+};
